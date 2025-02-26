2bc8f9ad183c0c5235ef130940f56b7adb9d4749
isaberjan420 <isaberjan420@gmail.com>
2025-02-26 18:51:50 +0300

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,1 +1,2 @@
-{"picklepicklestar", "mypankeyk_6789", "nomnomnomnomnojnoj", "gioatuk1234", "gioatuk9999", "gioatuk2564"}
+getgenv().Key = "amogus"
+getgenv().Whitelist = {"picklepicklestar", "mypankeyk_6789", "nomnomnomnomnojnoj", "gioatuk1234", "gioatuk9999", "gioatuk2564", "Holuplays2011"}
